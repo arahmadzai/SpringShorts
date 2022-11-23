3b8d050b07625b41df8dd6e2c29191f1213b169a
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-11-23 17:33:51 -0500

    IoC container Configuration classes created

---
+++ Spring-01-IoC/src/test/java/com/cydeo/bean_annotation/Spring01IoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.bean_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01IoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
