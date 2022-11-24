b4017a040014ca4bfe02f1397e585760076a65dd
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-11-24 00:35:34 -0500

    @Qualifier

--- Spring-02-Qualifier/src/main/java/com/cydeo/service/JavaService.java
+++ Spring-02-Qualifier/src/main/java/com/cydeo/service/JavaService.java
@@ -7,18 +7,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class JavaService{
 
-    private final ExtraSessions extraSessions;
+    private final ExtraSessions mentorsHours;
+    private final ExtraSessions officeHours;
     private final JavaRepository javaRepository;
 
-    @Autowired
-    public JavaService(@Qualifier("officeHoursService") ExtraSessions extraSessions, JavaRepository javaRepository) {
-        this.extraSessions = extraSessions;
+    public JavaService(@Qualifier("Mentor") ExtraSessions mentorsHours, @Qualifier("Office") ExtraSessions officeHours, JavaRepository javaRepository) {
+        this.mentorsHours = mentorsHours;
+        this.officeHours = officeHours;
         this.javaRepository = javaRepository;
     }
 
+    @Autowired
+
+
     public void getTeachingHours(){
 
-        System.out.println("Weekly Java Hours: " + (javaRepository.returnHours() + extraSessions.getHours()));
+        System.out.println("Weekly Java Hours: " + (javaRepository.returnHours() + officeHours.getHours() + mentorsHours.getHours()));
 
     }
 }
--- Spring-02-Qualifier/src/main/java/com/cydeo/service/OfficeHoursService.java
+++ Spring-02-Qualifier/src/main/java/com/cydeo/service/OfficeHoursService.java
@@ -1,8 +1,10 @@
 package com.cydeo.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("Office")
 public class OfficeHoursService implements ExtraSessions{
 
     @Override
