9397068e3a2aa870440b9dc7ce4fd1ab0dc68fe4
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-11-24 02:54:19 -0500

    MVC created POJO and used Model interface to carry it to view

--- Spring-04-MVC-Model/src/main/java/com/cydeo/controller/MainController.java
+++ Spring-04-MVC-Model/src/main/java/com/cydeo/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,5 +14,14 @@ public class MainController {
         model.addAttribute("name","Abidullah Rahimi");
 
         return "employee-list.html";
+    }
+
+    @RequestMapping("/employee-info")
+    public String empInfo(Model model){
+
+        Employee emp1 = new Employee("Abidullah", "Rahimi", 250000);
+        model.addAttribute("emp", emp1);
+
+        return "employee-info.html";
     }
 }
