f985f34763e77354c13e1aaa4b2062aa13e88961
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-11-24 23:09:22 -0500

    MVC Thymeleaf list iteration and web tables

--- Spring-04-MVC-Model/src/main/java/com/cydeo/controller/MainController.java
+++ Spring-04-MVC-Model/src/main/java/com/cydeo/controller/MainController.java
@@ -1,19 +1,40 @@
 package com.cydeo.controller;
 
 import com.cydeo.model.Employee;
+import com.github.javafaker.Faker;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
+
+
+
     @RequestMapping("/employees")
     public String empList(Model model){
+        List<Employee> employeeList = new ArrayList<>();
+        Faker faker = new Faker();
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
+        employeeList.add(new Employee(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(100000, 150000)));
 
-        model.addAttribute("name","Abidullah Rahimi");
+        model.addAttribute("employees",employeeList);
 
-        return "employee-list.html";
+        return "/employee-list";
     }
 
     @RequestMapping("/employee-info")
