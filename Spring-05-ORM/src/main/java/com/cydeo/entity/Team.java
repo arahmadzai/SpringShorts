b3c9287c933b287f94d92489adb3404b29fbd314
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-11-26 22:44:55 -0500

    Table Created with @Entity springboot

---
+++ Spring-05-ORM/src/main/java/com/cydeo/entity/Team.java
@@ -0,0 +1,25 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Country;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "teams")
+public class Team extends BaseEntity{
+
+    @Column(name = "teamName")
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    @Transient
+    private String founder;
+}
